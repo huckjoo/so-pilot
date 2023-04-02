a70d3235ed1bb8e493c5e899b27e4d01404e3585
huckjoo96 <omp0109@naver.com>
2023-04-02 17:29:01 +0900

    feat: 다른 레포에서 버튼 import 확인

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from '@dcron/common';
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>모노레포 버튼</h1>
+      <Button textColor='red'>Common Package Button</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ packages/common/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import type {FC, PropsWithChildren} from 'react';
+
+interface Props extends PropsWithChildren{
+  textColor: string;
+}
+
+const Button: FC<Props> = (props) => {
+  return (
+    <button style={{color: props.textColor}}>{props.children}</button>
+  );
+}
+
+export default Button
